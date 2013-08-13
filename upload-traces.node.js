99f540359c589bf7f44e6b0aad7c7864781ae0cb
Andrius Aucinas <andrius.aucinas@gmail.com>
2013-08-13 16:42:25 +0300

    node upload backend

---
+++ upload-traces.node.js
@@ -0,0 +1,27 @@
+var express = require("express"),                                                                
+    app = express();                                                                             
+
+// tell express to use the bodyParser middleware                                                 
+// and set upload directory                                                                      
+app.use(express.bodyParser({ keepExtensions: true, uploadDir: "uploads" }));                     
+app.engine('jade', require('jade').__express);                                                   
+
+app.post("/upload", function (request, response) {                                               
+    // request.files will contain the uploaded file(s),                                          
+    // keyed by the input name (in this case, "file")                                            
+
+    // show the uploaded file name
+    console.log("file name", request.files.file.name);
+    console.log("file path", request.files.file.path);
+    console.log("request");
+    console.log(request)
+
+    response.end("upload complete");
+});                                                                                              
+
+// render file upload form                                                                       
+app.get("/upload-traces", function (request, response) {                                                      
+    response.render("upload_form.jade");                                                         
+});                                                                                              
+
+app.listen(3000);
